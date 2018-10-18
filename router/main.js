9c23f6894f68fac60a3d81205c0670f5c785012b
Ambuj <thecoderguy@outlook.com>
2018-10-18 15:45:41 +0530

    include ejs and minor changes in form

--- router/main.js
+++ router/main.js
@@ -1,22 +1,23 @@
-
+var express = require('express');
+var app = express();
 var Contact = require("./../models/Schema");
-
+app.set('view engine', 'ejs')
 module.exports = function(app){
     app.get("/",(req,res)=>{
-        res.render("index.html");
+        res.render("index");
     });
     app.get("/about",(req,res)=>{
-        res.render("about.html");
+        res.render("about");
     });
     app.get("/contact",(req,res)=>{
         Contact.find({},{},function(err,docs){
-            res.render("contact.html",{contacts:docs});
+            res.render("contact",{contacts:docs});
          });
            // res.render("contact.html");
       
     });
     app.get("/chat",(req,res)=>{
-        res.render("chat.html");
+        res.render("chat");
     });
    app.post("/",(req,res)=>{
     res.send(req.body.comment);
@@ -27,7 +28,7 @@ module.exports = function(app){
      Contact.create(data,function(err, record){
          if(err) throw err;
          Contact.find({},{},function(err,docs){
-            res.render("contact.html",{contacts:docs});
+            res.render("contact",{contacts:docs});
          });
 
      });
